776fa4971531d073dda2f897babbf9653565f557
Arun-Code932 <arunkumarak0804@gmail.com>
2026-07-13 17:58:49 +0530

    Added automatic model downloading for deployment

--- download_models.py
+++ download_models.py
@@ -3,19 +3,19 @@ import gdown
 
 os.makedirs("models", exist_ok=True)
 
-files = {
+FILES = {
     "models/similarity.pkl": "1xh4YzKFBhaF7gvoljuBGUSrhl_ck9zWm",
     "models/collaborative_similarity.pkl": "1JS4lBp960Wm8PYNlgHloxKOa2CE3NmoY",
 }
 
-for path, file_id in files.items():
+for path, file_id in FILES.items():
 
     if not os.path.exists(path):
 
-        print(f"Downloading {path}...")
+        print(f"Downloading {os.path.basename(path)}...")
 
         url = f"https://drive.google.com/uc?id={file_id}"
 
         gdown.download(url, path, quiet=False)
 
-print("All models downloaded successfully.")
+print("✅ All models downloaded successfully.")
